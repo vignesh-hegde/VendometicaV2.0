bca75695c697f348e3d4158806445b86acc761ff
vignesh-hegde <vignesh@Vignesh-Galaxy-Book-2-Pro>
2025-05-11 06:50:22 +0530

    Initial Heater function added

---
+++ src/WaterHeater.cpp
@@ -0,0 +1,61 @@
+#include "WaterHeater.hpp"
+
+cWaterHeater::cWaterHeater() : mMaxHeatingTemperature(0), mIdleTemperature(0), mpHeaterTimer(NullPtr), mIsHeaterEnabled(true)
+{
+    smpInstance = NullPtr;
+    mpOneWire = new cOneWire(TEMPERATURE_SENSOR);
+    mpTemperatureSensors = new cDallasTemperature(mpOneWire);
+    pinMode(HEATER_COIL, OUTPUT);
+    StopHeater();
+}
+
+cWaterHeater::~cWaterHeater()
+{
+        delete mpOneWire;
+        delete mpTemperatureSensors;
+        mpOneWire = NullPtr;
+        mpTemperatureSensors = NullPtr;
+}
+
+void IRAM_ATTR cWaterHeater::SetAlarmCallback()
+{
+    if(smpInstance != NULL)
+    {
+        smpInstance->SetAlarm();
+    }
+}
+
+void IRAM_ATTR cWaterHeater::SetAlarm()
+{
+    // Stop power supply to heating Coil
+    DisableHeater();
+    // IEC104 Send ERROR TO SERVER
+}
+
+UInt32 cWaterHeater::GetWaterTemperatureC(void)
+{
+    return (UInt32)(mpTemperatureSensors->getTempCByIndex(0));
+}
+
+void cWaterHeater::StartHeater()
+{
+    StopHeater();
+    if (mIsHeaterEnabled)
+    {
+        smpInstance = this;
+        
+        mpHeaterTimer = timerBegin(0, 80, true);
+        timerAttachInterrupt(mpHeaterTimer, &(cWaterHeater::SetAlarmCallback), true);
+        timerAlarmWrite(mpHeaterTimer, MAX_ALLOWED_HEATING_PERIOD, true);
+        timerAlarmEnable(mpHeaterTimer);
+        
+        digitalWrite(HEATER_COIL, HIGH);
+        while (mMaxHeatingTemperature > GetWaterTemperatureC());
+        StopHeater();
+    }
+}
+
+void cWaterHeater::StopHeater()
+{
+    digitalWrite(HEATER_COIL, LOW);
+}
---
+++ src/WaterHeater.hpp
@@ -0,0 +1,37 @@
+#ifndef WaterHeater_H
+#define WaterHeater_H
+#define MAX_ALLOWED_HEATING_PERIOD (1000000 * MAX_ALLOWED_HEATING_PERIOD_SEC)
+#include "VendometicaConfig.hpp"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+typedef OneWire cOneWire;
+typedef DallasTemperature cDallasTemperature;
+
+class cWaterHeater
+{
+private:
+
+    hw_timer_t *mpHeaterTimer; 
+    Float32 mIdleTemperature;
+    Float32 mMaxHeatingTemperature;
+    bool mIsHeaterEnabled;
+    cOneWire *mpOneWire;
+    cDallasTemperature *mpTemperatureSensors;
+    void DisableHeater(){mIsHeaterEnabled = false;}
+    UInt32 GetWaterTemperatureC(void);
+    void SetAlarm();
+    static void IRAM_ATTR SetAlarmCallback(); 
+    static cWaterHeater *smpInstance;
+
+public:
+    cWaterHeater();
+    ~cWaterHeater();
+    
+    inline void SetIdleTemperatureC(Float32 Temperature){ mIdleTemperature = Temperature;}
+    void SetHeatingTemperatureC(Float32 Temperature){ mMaxHeatingTemperature = Temperature;}
+    void StartHeater();
+    void StopHeater();
+};
+
+#endif
